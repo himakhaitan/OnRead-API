b823e0b9a2221784ac81e7803500a3839254db0e
Himanshu Khaitan <himanshukhaitan108@gmail.com>
2022-06-09 02:19:22 +0530

    Routes added

--- api/admin/books.js
+++ api/admin/books.js
@@ -49,4 +49,25 @@ router.post("/new", async (req, res) => {
   return res.status(200).json(newBook);
 });
 
+/*
+METHOD: GET
+URL: /api/books/:id
+DESCRIPTION: Route to get a book by id
+ACCESS: PUBLIC
+*/
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  // Find the book by id
+  const book = await Book.findByPk(parseInt(id));
+  if (!book) {
+    // Resource not found
+    res.status(404).json({
+      error: "Book not found",
+    });
+  }
+  res.status(200).json(book);
+});
+
 module.exports = router;
